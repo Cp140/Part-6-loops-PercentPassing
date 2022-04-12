2390f99b382e368338ecb8424228d5fb3c529cf5
Cole Palmer <PalmCole551@GoTVDSB.ca>
2022-04-12 10:53:47 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Part_6__loops_PercentPassing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Please enter an integer:");
+            while (Int32.TryParse(Console.ReadLine(), out                                                                                                                                                                                                                                                                                                                                    number))
+                Console.WriteLine("Invalid integer, try again.");
+
+
+
+            Console.WriteLine($"Thanks for {number}!");
+        }
+    }
+}
